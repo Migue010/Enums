1a2da87fe83cf4645f37048e335c076025f460fd
user.mail <migueblanci28@gmail.com>
2024-09-30 10:34:05 -0500

    Enums

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        enum MarcasAutos{
+            Audi , BMW , TOYOTA , RENAULT , KIA , FERRARI
+        }
+
+        List<MarcasAutos> marcasAutos = new ArrayList<>();
+
+       // marcasAutos.add(MarcasAutos.BMW);
+        //System.out.println(marcasAutos.toString());
+
+        for (Papeleria P : Papeleria.values()){
+            System.out.println(P.name());
+            System.out.println("CANTIDAD:" + P.getUnidades());
+            System.out.println("PRECIO: " +P.getCosto());
+        }
+
     }
 }
--- src/Papeleria.java
+++ src/Papeleria.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
-
 public enum Papeleria {
+    Calculadora(10,15000),
+    Cuadernos(50 ,4500),
+    Lapices(100 , 1200),
+    Carpetas(10 , 3000);
+
+
+    private int unidades;
+    private double costo;
+
+    Papeleria(int unidades, double costo) {
+        this.unidades = unidades;
+        this.costo = costo;
+    }
+
+    @Override
+    public String toString() {
+        return "Papeleria{" +
+                "unidades=" + unidades +
+                ", costo=" + costo +
+                '}';
+    }
+
+    public int getUnidades() {
+        return unidades;
+    }
+
+    public void setUnidades(int unidades) {
+        this.unidades = unidades;
+    }
+
+    public double getCosto() {
+        return costo;
+    }
+
+    public void setCosto(double costo) {
+        this.costo = costo;
+    }
 }
